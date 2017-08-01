c79e21aef2b31af81a6cf8d6b3f058ad6c7a317b
Finn Terdal <fterdal@gmail.com>
2017-08-01 09:54:34 -0500

    Start to refactor to a routes dir, rather than the app.js

--- app.js
+++ app.js
@@ -3,38 +3,35 @@ const nunjucks = require('nunjucks');
 const express = require('express');
 const app = express();
 
+const tweetBank = require('./tweetBank');
+const routes = require('./routes');
+app.use('/', routes);
+
 // Nunjucks Config
 app.set("view engine","html");
 app.engine('html', nunjucks.render);
 nunjucks.configure('views', {noCache: true});
 
-// Sample Data
-var locals = {
-  title: 'An Example',
-  people: [
-    { name: 'Gandalf'},
-    { name: 'Frodo' },
-    { name: 'Hermione'}
-  ]
-};
+app.use('/', routes);
 
-
-app.use('*', (req, res, next) => {
-  console.log(chalk.blue(req.method), chalk.red(req.originalUrl));
-  next();
-});
-
-app.get('/', (req, res) => {
-  const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-  res.render( 'index', {title: 'Hall of Fame', people: people} );
-});
-
-app.get('/news', (req, res) => {
-  res.send('BREAKING NEWS!');
-});
+// app.get('/', (req, res) => {
+//   const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+//   res.render( 'index', {title: 'Hall of Fame', people: people} );
+// });
+//
+// app.get('/tweets', (req, res) => {
+//   // const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+//   const tweets = tweetBank.list();
+//   res.render('index', {title: "List of Tweets", people: tweets});
+// });
+//
+// app.post('/tweets', (req, res) => {
+//   // const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+//   const newTweet = { name: "Finn Ashish", content: "Here's a successful POST!" };
+//   tweetBank.add(newTweet.name, newTweet.content);
+//   res.render( 'index', {title: 'Hall of Fame', people: people} );
+// });
 
 const PORT = 3000;
 // Listeneing to server
-app.listen(3000, () => {
-  console.log(`Listening on PORT ${PORT}`);
-});
+app.listen(PORT, () => { } );
