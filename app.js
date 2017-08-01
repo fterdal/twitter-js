fbee19d5f96e4737dab6d08cb24fe6728a291dd2
Finn Terdal <fterdal@gmail.com>
2017-08-01 10:07:33 -0500

    Adds bootstrap styling

--- app.js
+++ app.js
@@ -14,24 +14,6 @@ nunjucks.configure('views', {noCache: true});
 
 app.use('/', routes);
 
-// app.get('/', (req, res) => {
-//   const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-//   res.render( 'index', {title: 'Hall of Fame', people: people} );
-// });
-//
-// app.get('/tweets', (req, res) => {
-//   // const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-//   const tweets = tweetBank.list();
-//   res.render('index', {title: "List of Tweets", people: tweets});
-// });
-//
-// app.post('/tweets', (req, res) => {
-//   // const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-//   const newTweet = { name: "Finn Ashish", content: "Here's a successful POST!" };
-//   tweetBank.add(newTweet.name, newTweet.content);
-//   res.render( 'index', {title: 'Hall of Fame', people: people} );
-// });
-
 const PORT = 3000;
 // Listeneing to server
 app.listen(PORT, () => { } );
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
+const fs = require('fs');
 
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
   res.render( 'index', { tweets: tweets } );
+});
+
+router.get('/stylesheets/style.css', function (req, res) {
+  // let css = fs.readFileSync('../public/stylesheets/style.css');
+  res.sendFile('/code/fullstack/junior/twitter-js/public/stylesheets/style.css');
 });
 
 module.exports = router;
